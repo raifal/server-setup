3c321e889e61846a630af57d77a705c09366a580
Rainer Faller <mail@rainer-faller.de>
2017-10-09 22:03:15 +0200

    minor database adjustments

--- 2_rebuild_database.sh
+++ 2_rebuild_database.sh
@@ -8,7 +8,7 @@ sudo docker exec -t hsm-database  pg_dumpall -c -U postgres > mydump.sql
 sudo docker stop hsm-database
 sudo docker rm -v hsm-database
 
-sudo docker run --name hsm-database -e POSTGRES_PASSWORD=${postgres.password} -d postgres:10.0
+sudo docker run --name hsm-database -e POSTGRES_PASSWORD=${postgres_password} -d postgres:10.0
 sudo docker network connect rf_network hsm-database
 
 #restore
