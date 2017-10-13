c2181125a7169202e223f92eb3879949c93d76fe
Rainer Faller <mail@rainer-faller.de>
2017-10-13 22:17:30 +0200

    exposing port 7071

--- 1_rebuild_apache.sh
+++ 1_rebuild_apache.sh
@@ -17,7 +17,7 @@ sudo docker network create rf_network
 sudo docker build --build-arg apache_httpd_passwd_user=${apache_httpd_passwd_user} --build-arg apache_httpd_passwd_password=${apache_httpd_passwd_password} --build-arg arduino_interface_passwd_user=${arduino_interface_passwd_user} --build-arg arduino_interface_passwd_password=${arduino_interface_passwd_password} -t rf_apache_httpd  apache_httpd/.
 
 
-sudo docker run --restart='always' -d --name=rf_apache_httpd -p 80:80 -p 443:443 rf_apache_httpd
+sudo docker run --restart='always' -d --name=rf_apache_httpd -p 80:80 -p 443:443 -p 7071:7071 rf_apache_httpd
 sudo docker network connect rf_network rf_apache_httpd
 
 # sudo docker run -it --rm --link rf_jenkins_master --name=rf_apache_httpd -p 80:80 -p 443:443 rf_apache_httpd /bin/bash
