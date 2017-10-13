90321fa08c6fec22864e00b6424be68e267d3ecd
Rainer Faller <mail@rainer-faller.de>
2017-10-13 21:43:42 +0200

    added arduino endpoint

--- 1_rebuild_apache.sh
+++ 1_rebuild_apache.sh
@@ -14,7 +14,8 @@ sudo docker rmi rf_apache_httpd
 sudo docker network rm rf_network
 sudo docker network create rf_network
 
-sudo docker build --build-arg apache_httpd_passwd_user=${apache_httpd_passwd_user} --build-arg apache_httpd_passwd_password=${apache_httpd_passwd_password} -t rf_apache_httpd  apache_httpd/.
+sudo docker build --build-arg apache_httpd_passwd_user=${apache_httpd_passwd_user} --build-arg apache_httpd_passwd_password=${apache_httpd_passwd_password} --build-arg arduino_interface_passwd_user=${arduino_interface_passwd_user} --build-arg arduino_interface_passwd_password=${arduino_interface_passwd_password} -t rf_apache_httpd  apache_httpd/.
+
 
 sudo docker run --restart='always' -d --name=rf_apache_httpd -p 80:80 -p 443:443 rf_apache_httpd
 sudo docker network connect rf_network rf_apache_httpd
