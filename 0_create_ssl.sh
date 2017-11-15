bd3e5e6b557ffa581724a53be437335750e03e3f
Rainer Faller <raifal@users.noreply.github.com>
2017-11-15 22:59:14 +0100

    separated keystore and private key generation

--- 0_create_ssl.sh
+++ 0_create_ssl.sh
@@ -9,9 +9,9 @@ sudo docker rm -v temp-ssl
 sudo docker build  --build-arg ssl_storepass=${ssl_storepass} -t temp-ssl  ssl/.
 sudo docker run --name=temp-ssl temp-ssl
 
-sudo docker cp temp-ssl:/etc/ssl/private/ss_server.key ./ssl_generated/.
-sudo docker cp temp-ssl:/etc/ssl/certs/ss_server.crt ./ssl_generated/.
-sudo docker cp temp-ssl:/hsm.keystore ./ssl_generated/.
+sudo docker cp temp-ssl:/etc/ssl/private/ss_server.key ./apache_http/ssl_generated/.
+sudo docker cp temp-ssl:/etc/ssl/certs/ss_server.crt ./apache_http/ssl_generated/.
+sudo docker cp temp-ssl:/hsm.keystore ./apache_http/ssl_generated/.
 
 sudo docker stop temp-ssl
 sudo docker rm -v temp-ssl
--- 1_rebuild_apache.sh
+++ 1_rebuild_apache.sh
@@ -4,8 +4,8 @@ export $(cut -d= -f1 ../secrets/secrets.env)
 sudo docker network disconnect rf_network rf_apache_httpd
 sudo docker network disconnect rf_network hsm-database
 
-sudo docker stop hsm-database
-sudo docker rm -v hsm-database
+# sudo docker stop hsm-database
+# sudo docker rm -v hsm-database
 
 sudo docker stop rf_apache_httpd
 sudo docker rm -v rf_apache_httpd
@@ -19,6 +19,8 @@ sudo docker build --build-arg apache_httpd_passwd_user=${apache_httpd_passwd_use
 
 sudo docker run --restart='always' -d --name=rf_apache_httpd -p 80:80 -p 443:443 -p 7071:7071 rf_apache_httpd
 sudo docker network connect rf_network rf_apache_httpd
+
+sudo docker network connect rf_network hsm-database
 
 sudo docker cp ./apache_httpd/html rf_apache_httpd:/var/www
 
