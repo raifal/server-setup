cdb8a06b154cbb57cac12aa1b13d9e118aa67af6
Rainer Faller <raifal@users.noreply.github.com>
2017-11-15 23:01:42 +0100

    separated keystore and private key generation

--- 0_create_ssl.sh
+++ 0_create_ssl.sh
@@ -1,7 +1,7 @@
 source ../secrets/secrets.env
 export $(cut -d= -f1 ../secrets/secrets.env)
 
-mkdir ./apache_http/ssl_generated
+mkdir ./apache_httpd/ssl_generated
 
 sudo docker stop temp-ssl
 sudo docker rm -v temp-ssl
@@ -9,9 +9,9 @@ sudo docker rm -v temp-ssl
 sudo docker build  --build-arg ssl_storepass=${ssl_storepass} -t temp-ssl  ssl/.
 sudo docker run --name=temp-ssl temp-ssl
 
-sudo docker cp temp-ssl:/etc/ssl/private/ss_server.key ./apache_http/ssl_generated/.
-sudo docker cp temp-ssl:/etc/ssl/certs/ss_server.crt ./apache_http/ssl_generated/.
-sudo docker cp temp-ssl:/hsm.keystore ./apache_http/ssl_generated/.
+sudo docker cp temp-ssl:/etc/ssl/private/ss_server.key ./apache_httpd/ssl_generated/.
+sudo docker cp temp-ssl:/etc/ssl/certs/ss_server.crt ./apache_httpd/ssl_generated/.
+sudo docker cp temp-ssl:/hsm.keystore ./apache_httpd/ssl_generated/.
 
 sudo docker stop temp-ssl
 sudo docker rm -v temp-ssl
